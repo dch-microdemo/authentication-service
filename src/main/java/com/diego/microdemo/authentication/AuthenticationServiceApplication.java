7fe4265fcb910f8698064c3b831eb5dc85480519
diegochavezcarro <diegochavezcarro@gmail.com>
2019-05-29 17:54:48 -0300

    Primera Subida

---
+++ src/main/java/com/diego/microdemo/authentication/AuthenticationServiceApplication.java
@@ -0,0 +1,15 @@
+package com.diego.microdemo.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+public class AuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/diego/microdemo/authentication/security/JWTOAuth2Config.java
@@ -0,0 +1,43 @@
+package com.diego.microdemo.authentication.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+@Configuration
+public class JWTOAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private TokenStore tokenStore;
+
+	@Autowired
+	private JwtAccessTokenConverter jwtAccessTokenConverter;
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
+		endpoints.tokenStore(tokenStore) // JWT
+				.accessTokenConverter(jwtAccessTokenConverter) // JWT
+				.authenticationManager(authenticationManager).userDetailsService(userDetailsService);
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+		clients.inMemory().withClient("aplicacion").secret("{noop}1234")
+				.authorizedGrantTypes("refresh_token", "password", "client_credentials")
+				.scopes("webclient", "mobileclient");
+	}
+}
